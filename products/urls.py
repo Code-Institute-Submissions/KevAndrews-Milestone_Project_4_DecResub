892c5aecf3d3627768e9f6b6880d770a85cd5b0f
Kevin Andrews <nivek87@gmail.com>
2021-12-04 14:29:42 +0000

    Add search bar toggle template

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ from products.views import AllGames, GameDetail
 
 urlpatterns = [
     path('', AllGames.as_view(), name='games'),
-    path('<game_id>', GameDetail.as_view(), name='game_detail'),
+    path('<int:game_id>/', GameDetail.as_view(), name='game_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,13 @@
 """
 Class views for the Games / Products
 """
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+from django.contrib.auth.decorators import login_required
 from django.views import View
-from django.shortcuts import render, get_object_or_404
-from .models import Game
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Game, Category
 
 
 class AllGames(View):
@@ -18,11 +22,48 @@ class AllGames(View):
         games = Game.objects.all()
         query = None
         categories = None
+        sort = None
+        direction = None
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                games = games.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category_name':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            games = games.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            games = games.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('games'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            games = games.filter(queries)
+
+        current_sorting = f'{sort}_{direction}'
 
         context = {
             'games': games,
             'search_term': query,
             'current_categories': categories,
+            'current_sorting': current_sorting,
         }
 
         return render(request, 'products/games.html', context)
