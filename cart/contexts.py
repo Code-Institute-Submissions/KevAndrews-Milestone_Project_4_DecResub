e021a8a713cac65624a9807ea4fe362559f67479
Kevin Andrews <nivek87@gmail.com>
2021-12-05 15:01:24 +0000

    Update add to cart function across the site

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,3 +1,6 @@
+"""
+Context for the Carts content
+"""
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -5,20 +8,24 @@ from products.models import Game
 
 
 def cart_contents(request):
-
+    """
+    Context for the Carts content
+    Code taken from Boutique Ado project
+    Modified to work with this project
+    """
     cart_items = []
     total = 0
     game_count = 0
     cart = request.session.get('cart', {})
 
-    for item_id, item_data in cart.items():
-        if isinstance(item_data, int):
-            game = get_object_or_404(Game, pk=item_id)
-            total += item_data * game.price
-            game_count += item_data
+    for game_id, game_data in cart.items():
+        if isinstance(game_data, int):
+            game = get_object_or_404(Game, pk=game_id)
+            total += game_data * game.price
+            game_count += game_data
             cart_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
+                'game_id': game_id,
+                'quantity': game_data,
                 'product': game,
             })
 
