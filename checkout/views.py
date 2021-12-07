b9cc4ba036d3d6f271dd296ab3e6988bb1a3047f
Kevin Andrews <nivek87@gmail.com>
2021-12-07 20:49:50 +0000

    Setting up Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,14 @@
 """
 Class views for the Checkout
 """
+import stripe
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.views import View
+from django.conf import settings
 
+from cart.contexts import cart_contents
 from .forms import OrderForm
 
 
@@ -19,16 +23,34 @@ class Checkout(View):
         Used Boutique Ado project for logic and updated it
         to work in this project
         """
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+
         cart = request.session.get('cart', {})
         if not cart:
             messages.error(
                 request, "There's nothing in your cart at the moment")
             return redirect(reverse('products'))
 
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
         order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
 
         context = {
             'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': stripe_secret_key
         }
 
         return render(request, 'checkout/checkout.html', context)
