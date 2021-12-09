2ebd9440ca0d3c8e3b7d96ce9a748dea4f2a4c95
Kevin Andrews <nivek87@gmail.com>
2021-12-09 22:25:59 +0000

    Added confirmation emails

--- checkout/views.py
+++ checkout/views.py
@@ -37,7 +37,7 @@ class Checkout(View):
         if not cart:
             messages.error(
                 request, "There's nothing in your cart at the moment")
-            return redirect(reverse('products'))
+            return redirect(reverse('games'))
 
         current_cart = cart_contents(request)
         total = current_cart['grand_total']
