9c3fe05bc9dd4ba05125323695b3c3a6afff6957
Kevin Andrews <nivek87@gmail.com>
2021-12-09 21:54:03 +0000

    Added removing profile inputs

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ from cart.contexts import cart_contents
 from products.models import Game
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 
 
 class Checkout(View):
@@ -47,7 +49,24 @@ class Checkout(View):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
         if not stripe_public_key:
             messages.warning(request, 'Stripe public key is missing. \
@@ -140,6 +159,7 @@ class CheckoutSuccess(View):
         save_info = request.session.get('save_info')
         order = get_object_or_404(Order, order_number=order_number)
 
+        
         messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
