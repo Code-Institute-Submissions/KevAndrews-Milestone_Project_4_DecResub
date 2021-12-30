25f521669f6ad324f8bcf8bddd84d4fc4cefeaab
Kevin Andrews <nivek87@gmail.com>
2021-12-30 19:31:29 +0000

    Update debug to true

--- reviews/apps.py
+++ reviews/apps.py
@@ -1,6 +1,12 @@
+"""
+Apps Class for Review
+"""
 from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
+    """
+    Apps Class for Review
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
